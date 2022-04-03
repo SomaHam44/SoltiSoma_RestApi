e2642fd0231bf9c6d4127a416f08013b509d6bdb
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2022-04-03 20:02:44 +0200

    ListResultActivity, InsertActivity kinézetek, MainActivityre visszatérő gombok

--- app/src/main/java/hu/petrik/soltisoma_restapi/InsertActivity.java
+++ app/src/main/java/hu/petrik/soltisoma_restapi/InsertActivity.java
@@ -2,13 +2,36 @@ package hu.petrik.soltisoma_restapi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class InsertActivity extends AppCompatActivity {
+    private EditText editNev;
+    private EditText editOrszag;
+    private EditText editLakossag;
+    private Button btnFelvesz;
+    private Button btnVissza;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
+        init();
+        btnVissza.setOnClickListener(view -> {
+            Intent vissza = new Intent(InsertActivity.this, MainActivity.class);
+            startActivity(vissza);
+            finish();
+        });
+    }
+
+    private void init() {
+        editNev = findViewById(R.id.editNev);
+        editOrszag = findViewById(R.id.editOrszag);
+        editLakossag = findViewById(R.id.editLakossag);
+        btnFelvesz = findViewById(R.id.btnFelvetel);
+        btnVissza = findViewById(R.id.btnVissza);
     }
 }
--- app/src/main/java/hu/petrik/soltisoma_restapi/ListResultActivity.java
+++ app/src/main/java/hu/petrik/soltisoma_restapi/ListResultActivity.java
@@ -2,13 +2,31 @@ package hu.petrik.soltisoma_restapi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ListResultActivity extends AppCompatActivity {
+    private TextView textViewAdatok;
+    private Button btnVissza;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_result);
+        init();
+        btnVissza.setOnClickListener(view -> {
+            Intent vissza = new Intent(ListResultActivity.this, MainActivity.class);
+            startActivity(vissza);
+            finish();
+
+        });
+    }
+
+    private void init() {
+        textViewAdatok = findViewById(R.id.textviewOrszag);
+        btnVissza = findViewById(R.id.btnVisszafele);
     }
 }
